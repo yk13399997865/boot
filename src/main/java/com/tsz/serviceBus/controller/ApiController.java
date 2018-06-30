2b780654e7b395a471ca380e6b7d78453715a19d
yangkang <13399997865@163.com>
2018-06-30 18:58:44 +0800

    qqwe

--- src/main/java/com/tsz/serviceBus/controller/ApiController.java
+++ src/main/java/com/tsz/serviceBus/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.tsz.serviceBus.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
 
-    @RequestMapping({"/",""})
-    public Object index(){
+    @RequestMapping({"/", ""})
+    public Object index() {
         return "测试的顺利";
+    }
+
+    @GetMapping("/list")
+    public String hello() {
+        return "hello world";
     }
 }
